55105d21d4b02c9ebc7a5824b4ec3f891d51e7fb
pppp0722 <poj0722@naver.com>
2022-04-04 22:39:02 +0900

    프로그래머스/Level1/같은 숫자는 싫어

--- src/test014/Test14.java
+++ src/test014/Test14.java
@@ -2,14 +2,12 @@ package test014;
 import java.util.ArrayList;
 // 프로그래머스/Level1/같은 숫자는 싫어
 public class Test14 {
-
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 		int[] input1 = {1,1,3,3,0,1,1};
 		int[] output = sol.solution(input1);
 		for(int i : output) System.out.println(i);
 	}
-
 }
 
 class Solution {
