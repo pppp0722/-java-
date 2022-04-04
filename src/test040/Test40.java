6eece8164bec16f843f30f994dce01f332b473b6
pppp0722 <poj0722@naver.com>
2022-04-04 22:54:24 +0900

    프로그래머스/Level1/직사각형 별찍기

--- src/test040/Test40.java
+++ src/test040/Test40.java
@@ -1,6 +1,7 @@
 package test040;
 import java.util.Scanner;
 // 프로그래머스/Level1/직사각형 별찍기
+
 public class Test40 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
