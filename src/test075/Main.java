e4fb638cc5818646e5aef064740ec1c901ceb8c9
pppp0722 <poj0722@naver.com>
2022-04-12 20:55:42 +0900

    백준/3등급/10974번 모든순열

---
+++ src/test075/Main.java
@@ -0,0 +1,41 @@
+package test075;
+
+// 백준/3등급/10974번 모든순열
+
+import java.util.Scanner;
+
+public class Main {
+    static int length;
+    static int[] numArr;
+    static boolean[] visited;
+    static StringBuilder stringBuilder = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        length = scanner.nextInt();
+        visited = new boolean[length];
+        numArr = new int[length];
+
+        backTracking(0);
+        System.out.print(stringBuilder.toString());
+    }
+
+    public static void backTracking(int depth) {
+        if(depth == length) {
+            for(int num : numArr)
+                stringBuilder.append(num + " ");
+            stringBuilder.append("\n");
+
+            return;
+        }
+
+        for(int i = 0; i < length; i++) {
+            if(visited[i]) continue;
+
+            visited[i] = true;
+            numArr[depth] = i + 1;
+            backTracking(depth + 1);
+            visited[i] = false;
+        }
+    }
+}
