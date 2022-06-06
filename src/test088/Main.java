7d0f759674f6ccf0f7cd3c5118c6747877090ad9
pppp0722 <poj0722@naver.com>
2022-06-07 00:29:47 +0900

    백준/실버1/1931번 회의실 배정

--- src/test088/Main.java
+++ src/test088/Main.java
@@ -1,5 +1,6 @@
 package test088;
 // 백준/실버1/1931번 회의실 배정
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,10 +13,9 @@ public class Main {
         int num = Integer.parseInt(br.readLine());
         int[][] usages = new int[num][2];
         for (int i = 0; i < num; i++) {
-            int[] time = Arrays.stream(br.readLine().split(" "))
+            usages[i] = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-            usages[i] = time;
         }
 
         Arrays.sort(usages, (o1, o2) -> o1[1] != o2[1] ? o1[1] - o2[1] : o1[0] - o2[0]);
