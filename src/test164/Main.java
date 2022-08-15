c5647c4586032acd019419d191541e68362b29c6
ilhwan <poj0722@naver.com>
2022-08-16 00:24:00 +0900

    백준/실버3/2193 이친수

---
+++ src/test164/Main.java
@@ -0,0 +1,21 @@
+package test164;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+// 백준/실버3/2193 이친수
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] dp = new long[N + 1];
+        dp[1] = 1;
+        for(int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[N]);
+    }
+}
