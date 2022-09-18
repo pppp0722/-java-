f34248fd33b3459f85cc54c64c41a830d42a7697
ilhwan <poj0722@naver.com>
2022-09-18 20:05:54 +0900

    백준/골드5/9251 LCS

---
+++ src/test181/Main.java
@@ -0,0 +1,26 @@
+package test181;
+// 백준/골드5/9251 LCS
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        int[][] lcs = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    lcs[i][j] = lcs[i - 1][j - 1] + 1;
+                } else {
+                    lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
+                }
+            }
+        }
+        System.out.println(lcs[s1.length()][s2.length()]);
+    }
+}
