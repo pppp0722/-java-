c69a0afde906c4291c3d694103cabb795b1d3ee4
ilhwan <poj0722@naver.com>
2022-12-01 16:50:32 +0900

    :sparkles: DP/백준/실버1/11057 오르막 수

--- src/test209/Main.java
+++ src/test209/Main.java
@@ -11,17 +11,22 @@ public class Main {
     private static final int MOD = 10_007;
 
     public static void main(String[] args) throws IOException {
+        // 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        // 2차원 DP 배열 초기화 ([자릿수][마지막 자리 숫자])
         int[][] dp = new int[n + 1][TEN];
         for (int i = 0; i < TEN; i++) {
             dp[1][i] = 1;
         }
 
+        // DP
         for (int i = 2; i <= n; i++) {
+            // 이전 수 마지막 자리 숫자 -> j, 다음 수 마지막 자리 숫자 -> k
             for (int j = 0; j < TEN; j++) {
                 for (int k = j; k < TEN; k++) {
+                    // 다음 수의 마지막 자리 숫자 k가 가능한 경우를 모두 더해줌
                     dp[i][k] = (dp[i][k] + dp[i - 1][j]) % MOD;
                 }
             }
