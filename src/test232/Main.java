58be34be7d4b139bc15ff551e81355c16456d0d0
ilhwan <poj0722@naver.com>
2022-12-14 21:53:23 +0900

    :sparkles: DFS/백준/골드4/14267 회사 문화 1

---
+++ src/test232/Main.java
@@ -0,0 +1,62 @@
+package test232;
+// DFS/백준/골드4/14267 회사 문화 1
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        List<List<Integer>> graph = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        st = new StringTokenizer(br.readLine());
+        st.nextToken();
+        for (int i = 2; i <= n; i++) {
+            int parent = Integer.parseInt(st.nextToken());
+            graph.get(parent).add(i);
+        }
+
+        // 칭찬 초기화
+        int[] answer = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int node = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            answer[node] += weight;
+        }
+
+        // DFS
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[]{1, 0});
+        while (!stack.isEmpty()) {
+            int[] cur = stack.pop();
+            int curNode = cur[0];
+            int curWeight = answer[curNode];
+            int totalWeight = cur[1];
+            answer[curNode] += totalWeight;
+            for (int nextNode : graph.get(curNode)) {
+                stack.push(new int[]{nextNode, curWeight + totalWeight});
+            }
+        }
+
+        // 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(answer[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
