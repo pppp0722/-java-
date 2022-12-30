03e1f976a781b81daa272bee6cadee52d0afe3be
ilhwan <poj0722@naver.com>
2022-12-30 22:17:23 +0900

    :sparkles: 큐/프로그래머스/Level2/다리를 지나는 트럭

--- src/test072/Test72.java
+++ src/test072/Test72.java
@@ -1,49 +1,33 @@
 package test072;
+// 큐/프로그래머스/Level2/다리를 지나는 트럭
 
 import java.util.LinkedList;
 import java.util.Queue;
 
-// 프로그래머스/Level2/다리를 지나는 트럭
-public class Test72 {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int bridge_length = 2;
-        int weight = 10;
-        int[] truck_weights = {7,4,5,6};
-        int result = solution.solution(bridge_length, weight, truck_weights);
-        System.out.println(result);
-    }
-}
-
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> leftoverTrucks = new LinkedList<>();
 
-        for(int truckWeight : truck_weights) {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Integer> bridge = new LinkedList<>();
+        Queue<Integer> leftoverTrucks = new LinkedList<>();
+        for (int truckWeight : truck_weights) {
             leftoverTrucks.add(truckWeight);
         }
 
-        Queue<Integer> bridge = new LinkedList<>();
-        int answer = 0;
+        int answer = bridge_length;
         int totalWeight = 0;
-        while(true) {
-            if(leftoverTrucks.isEmpty()) break;
-
-            if(bridge.size() == bridge_length) {
+        while (!leftoverTrucks.isEmpty()) {
+            if (bridge.size() == bridge_length) {
                 totalWeight -= bridge.poll();
             }
-
-            if(totalWeight + leftoverTrucks.peek() <= weight) {
-                int newTruckWeight = leftoverTrucks.poll();
-                bridge.add(newTruckWeight);
-                totalWeight += newTruckWeight;
-            } else {
-                bridge.add(0);
+            int truckWeight = 0;
+            if (leftoverTrucks.peek() + totalWeight <= weight) {
+                truckWeight = leftoverTrucks.poll();
             }
-
+            bridge.offer(truckWeight);
+            totalWeight += truckWeight;
             answer++;
         }
 
-        return answer + bridge_length;
+        return answer;
     }
 }
