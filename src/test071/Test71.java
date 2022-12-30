7178bfa46dc0e22a68f92e06a457befc824775cc
ilhwan <poj0722@naver.com>
2022-12-30 22:02:33 +0900

    :sparkles: 큐/프로그래머스/Level2/프린터

--- src/test071/Test71.java
+++ src/test071/Test71.java
@@ -1,56 +1,34 @@
 package test071;
+// 큐/프로그래머스/Level2/프린터
 
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
-// 프로그래머스/Level2/프린터
-public class Test71 {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] priorities = {1, 1, 9, 1, 1, 1};
-        int location = 0;
-        int result = solution.solution(priorities, location);
-        System.out.println(result);
-    }
-}
-
 class Solution {
-    public int solution(int[] priorities, int location) {
-        Map<Integer, Integer> map = new HashMap<>();
-        Queue<Integer> queue = new LinkedList<>();
 
-        for(int i = 0; i < priorities.length; i++) {
-            map.put(i, priorities[i]);
-            queue.add(i);
+    public int solution(int[] priorities, int location) {
+        Queue<int[]> q = new LinkedList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < priorities.length; i++) {
+            q.offer(new int[]{i, priorities[i]});
+            pq.offer(priorities[i]);
         }
 
-        int answer = 1;
-        while(true) {
-            int front = queue.poll();
-            int frontPriority = map.get(front);
-
-            if(isHighestPrioriy(map, frontPriority)) {
-                if(front == location) break;
-                map.remove(front);
-                answer++;
+        int order = 1;
+        while (true) {
+            int[] cur = q.poll();
+            if (cur[1] != pq.peek()) {
+                q.offer(cur);
             } else {
-                queue.add(front);
+                if (cur[0] == location) {
+                    break;
+                }
+                pq.poll();
+                order++;
             }
         }
-
-        return answer;
-    }
-
-    public boolean isHighestPrioriy(Map<Integer, Integer> map, int priority) {
-        boolean ret = true;
-        if (map.values().stream()
-                .filter(v -> v > priority)
-                .count() > 0) {
-            ret = false;
-        }
-
-        return ret;
+        return order;
     }
 }
