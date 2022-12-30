57c3856c31a23801671c1f1cc1931d43e563c8ca
ilhwan <poj0722@naver.com>
2022-12-30 23:28:32 +0900

    :sparkles: 그리디/프로그래머스/Level3/디스크 컨트롤러

--- src/test073/Test73.java
+++ src/test073/Test73.java
@@ -1,42 +1,37 @@
 package test073;
+// 그리디/프로그래머스/Level3/디스크 컨트롤러
 
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
-// 프로그래머스/Level3/디스크 컨트롤러
-// 힙
-public class Test73 {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[][] jobs = {{0, 6}, {2, 8}, {3, 7}, {7, 1}, {11, 11}, {19, 25}, {30, 15}, {32, 6}, {40, 3}};
-        int result = solution.solution(jobs);
-        System.out.println(result);
-    }
-}
-
 class Solution {
+
     public int solution(int[][] jobs) {
-        int indexOfJobs = 0;
-        int endTime = 0;
-        int totalTime = 0;
-        int numberOfCompletedJobs = 0;
-
-        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-
-        while (numberOfCompletedJobs < jobs.length) {
-            while (indexOfJobs < jobs.length && jobs[indexOfJobs][0] <= endTime) {
-                pq.add(jobs[indexOfJobs++]);
+        PriorityQueue<int[]> pq1 = new PriorityQueue<>((o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
             }
+            return o1[0] - o2[0];
+        });
+        PriorityQueue<int[]> pq2 = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+        for (int[] job : jobs) {
+            pq1.offer(job);
+        }
 
-            if (pq.isEmpty()) {
-                endTime = jobs[indexOfJobs][0];
+        int time = pq1.peek()[0];
+        int totalTime = 0;
+        while (!pq1.isEmpty() || !pq2.isEmpty()) {
+            int[] cur;
+            if (pq2.isEmpty()) {
+                cur = pq1.poll();
+                time = cur[0];
             } else {
-                int[] job = pq.poll();
-                totalTime += endTime + job[1] - job[0];
-                endTime += job[1];
-                numberOfCompletedJobs++;
+                cur = pq2.poll();
+            }
+            time += cur[1];
+            totalTime += time - cur[0];
+            while (!pq1.isEmpty() && pq1.peek()[0] <= time) {
+                pq2.offer(pq1.poll());
             }
         }
 
