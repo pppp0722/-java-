6d1cbf2bc38f77e1421cb2de5e77da62928a21e8
ilhwan <poj0722@naver.com>
2023-01-01 12:46:29 +0900

    :sparkles: DFS,미니맥스/프로그래머스/Level3/사라지는 발판

--- src/test085/Test85.java
+++ src/test085/Test85.java
@@ -1,6 +1,56 @@
 package test085;
 
-// 프로그래머스/Level3/사라지는 발판
+// DFS,미니맥스/프로그래머스/Level3/사라지는 발판
+
+class Solution {
+
+    private final int[] DX = {-1, 0, 1, 0};
+    private final int[] DY = {0, 1, 0, -1};
+    private int[][] board;
+    private int n;
+    private int m;
+
+    public int solution(int[][] board, int[] aloc, int[] bloc) {
+        this.board = board;
+        this.n = board.length;
+        this.m = board[0].length;
+        return dfs(aloc[0], aloc[1], bloc[0], bloc[1]);
+    }
+
+    public int dfs(int px, int py, int ox, int oy) {
+        if (isLandless(px, py)) {
+            return 0;
+        }
+        int ret = 0;
+        for (int i = 0; i < 4; i++) {
+            int nx = px + DX[i];
+            int ny = py + DY[i];
+            if (isOOB(nx, ny) || isLandless(nx, ny)) {
+                continue;
+            }
+            board[px][py] = 0;
+            int val = dfs(ox, oy, nx, ny) + 1;
+            board[px][py] = 1;
+            if (ret % 2 == 0 && val % 2 == 1) {
+                ret = val;
+            } else if (ret % 2 == 1 && val % 2 == 1) {
+                ret = Math.min(val, ret);
+            } else if (ret % 2 == 0 && val % 2 == 0) {
+                ret = Math.max(val, ret);
+            }
+        }
+        return ret;
+    }
+
+    private boolean isOOB(int x, int y) {
+        return x < 0 || x >= n || y < 0 || y >= m;
+    }
+
+    private boolean isLandless(int x, int y) {
+        return board[x][y] == 0;
+    }
+}
+
 public class Test85 {
 
     public static void main(String[] args) {
@@ -10,49 +60,5 @@ public class Test85 {
         int[] bloc = {1, 2};
         int answer = solution.solution(board, aloc, bloc);
         System.out.println(answer);
-    }
-}
-
-class Solution {
-
-    int[][] board;
-    int N;
-    int M;
-    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-    public int solution(int[][] board, int[] aloc, int[] bloc) {
-        this.board = board;
-        this.N = board.length;
-        this.M = board[0].length;
-        return dfs(aloc[0], aloc[1], bloc[0], bloc[1]);
-    }
-
-    public int dfs(int plyrX, int plyrY, int opX, int opY) {
-        if (board[plyrX][plyrY] == 0) {
-            return 0;
-        }
-        int ret = 0;
-        for (int[] dir : dirs) {
-            int nextX = plyrX + dir[0];
-            int nextY = plyrY + dir[1];
-            if (isOOB(nextX, nextY) || board[nextX][nextY] == 0) {
-                continue;
-            }
-            board[plyrX][plyrY] = 0;
-            int val = dfs(opX, opY, nextX, nextY) + 1;
-            board[plyrX][plyrY] = 1;
-            if(ret % 2 == 0 && val % 2 == 1) {
-                ret = val;
-            } else if(ret % 2 == 0 && val % 2 == 0) {
-                ret = ret > val ? ret : val;
-            } else if(ret % 2 == 1 && val % 2 == 1) {
-                ret = ret < val ? ret : val;
-            }
-        }
-        return ret;
-    }
-
-    public boolean isOOB(int x, int y) {
-        return x < 0 || x >= N || y < 0 || y >= M;
     }
 }
