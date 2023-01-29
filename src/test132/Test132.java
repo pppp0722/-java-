e07815d22376f1869a784ea05fff83cda4e1473c
ilhwan <poj0722@naver.com>
2023-01-29 11:04:00 +0900

    :sparkles: 해시테이블/프로그래머스/Level4/호텔 방 배정

--- src/test132/Test132.java
+++ src/test132/Test132.java
@@ -1,46 +1,37 @@
 package test132;
-// 프로그래머스/Level4/호텔 방 배정
+// 해시테이블/프로그래머스/Level4/호텔 방 배정
 
 import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
 
-    Map<Long, Long> map = new HashMap<>();
+    private Map<Long, Long> map;
 
     public long[] solution(long k, long[] room_number) {
+        map = new HashMap<>();
         long[] answer = new long[room_number.length];
-
         for (int i = 0; i < room_number.length; i++) {
-            answer[i] = getRoom(room_number[i]);
+            answer[i] = findRoom(room_number[i]);
         }
-
         return answer;
     }
 
-    long getRoom(long want) {
+    private long findRoom(long want) {
         if (!map.containsKey(want)) {
-            map.put(want, want + 1);
-
+            map.put(want, findNearestRoom(want + 1));
             return want;
         }
-
-        long next = map.get(want);
-        long empty = getRoom(next);
+        long empty = findRoom(map.get(want));
         map.put(want, empty);
-
         return empty;
     }
-}
 
-public class Test132 {
-
-    public static void main(String[] args) {
-        int k = 10;
-        long[] room_number = {1, 3, 4, 1, 3, 1};
-        long[] answer = new Solution().solution(k, room_number);
-        for (long l : answer) {
-            System.out.print(l + " ");
+    private long findNearestRoom(long room) {
+        if (map.containsKey(room)) {
+            return findNearestRoom(map.get(room));
+        } else {
+            return room;
         }
     }
 }
