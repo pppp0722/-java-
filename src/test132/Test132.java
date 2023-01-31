dfcac25bc2460e80a77bb7161022a661dcef140c
ilhwan <poj0722@naver.com>
2023-01-31 15:57:24 +0900

    :sparkles: 유파/프로그래머스/Level4/호텔 방 배정

--- src/test132/Test132.java
+++ src/test132/Test132.java
@@ -6,32 +6,36 @@ import java.util.Map;
 
 class Solution {
 
-    private Map<Long, Long> map;
+    private final Map<Long, Long> map = new HashMap<>();
 
     public long[] solution(long k, long[] room_number) {
-        map = new HashMap<>();
-        long[] answer = new long[room_number.length];
+        long[] assignedRooms = new long[room_number.length];
         for (int i = 0; i < room_number.length; i++) {
-            answer[i] = findRoom(room_number[i]);
+            assignRoom(assignedRooms, i, room_number[i]);
         }
-        return answer;
+        return assignedRooms;
     }
 
-    private long findRoom(long want) {
-        if (!map.containsKey(want)) {
-            map.put(want, findNearestRoom(want + 1));
-            return want;
-        }
-        long empty = findRoom(map.get(want));
-        map.put(want, empty);
-        return empty;
+    private void assignRoom(long[] assignedRooms, int customerIdx, long want) {
+        long assignedRoom = find(want);
+        union(assignedRoom, assignedRoom + 1);
+        assignedRooms[customerIdx] = assignedRoom;
     }
 
-    private long findNearestRoom(long room) {
-        if (map.containsKey(room)) {
-            return findNearestRoom(map.get(room));
-        } else {
-            return room;
+    private void union(long num1, long num2) {
+        num1 = find(num1);
+        num2 = find(num2);
+        if (num1 != num2) {
+            map.put(num1, num2);
         }
+    }
+
+    private long find(long num) {
+        if (!map.containsKey(num)) {
+            return num;
+        }
+        long nextNum = find(map.get(num));
+        map.put(num, nextNum);
+        return nextNum;
     }
 }
