c15dab4721039a38cb63e4076228fc8685398ae2
ilhwan <poj0722@naver.com>
2023-02-04 21:26:58 +0900

    :sparkles: 구현,DFS/프로그래머스/Level3/외벽 점검

--- src/test139/Test139.java
+++ src/test139/Test139.java
@@ -1,92 +1,90 @@
 package test139;
-// 프로그래머스/Level3/외벽 점검
+// 구현,DFS/프로그래머스/Level3/외벽 점검
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
 
-    boolean[] visited;
-    int[] dist;
-    int answer;
-    List<Integer> workers = new ArrayList<>();
+    private int lenOfOuterWall;
+    private int[] weekPoints;
+    private int numOfWeekPoints;
+    private Integer[] distances;
+    private int maxNumOfFriends;
 
     public int solution(int n, int[] weak, int[] dist) {
-        this.dist = dist;
-        int weakLen = weak.length;
-        int distLen = dist.length;
-        visited = new boolean[distLen];
-        answer = distLen + 1;
-
-        // 원형 벽을 고려하기 위해 week 을 1씩 shift
-        for (int i = 0; i < weakLen; i++) {
-            int[] shiftWeek = new int[weakLen];
-            for (int j = 0; j < weakLen - i; j++) {
-                shiftWeek[j] = weak[j + i];
-            }
-            for (int j = weakLen - i; j < weakLen; j++) {
-                shiftWeek[j] = weak[j + i - weakLen] + n;
-            }
-
-            // 1명부터 최대 인원까지 DFS
-            for (int j = 1; j <= distLen; j++) {
-                dfs(0, j, shiftWeek);
-                if (j >= answer) {
-                    break;
-                }
-            }
-        }
-
-        // 고칠 수 없는 경우 -1 출력
-        return answer == distLen + 1 ? -1 : answer;
+        lenOfOuterWall = n;
+        weekPoints = weak;
+        numOfWeekPoints = weekPoints.length;
+        distances = Arrays.stream(dist).boxed().toArray(Integer[]::new);
+        Arrays.sort(distances, Comparator.reverseOrder());
+        maxNumOfFriends = dist.length;
+        return findMinNumOfFriends();
     }
 
-    void dfs(int depth, int maxDepth, int[] weak) {
-        // 순열이 완성되면 모든 벽을 고칠 수 있는지 확인
-        if (depth == maxDepth) {
-            // 고칠 수 있으면 정답 갱신
-            if (canFixAll(weak)) {
-                answer = Math.min(depth, answer);
+    private int findMinNumOfFriends() {
+        int minNumOfFriends = -1;
+        for (int i = 1; i <= maxNumOfFriends; i++) {
+            boolean[] visited = new boolean[i];
+            int[] order = new int[i];
+            if (dfs(i, 0, visited, order)) {
+                minNumOfFriends = i;
+                break;
             }
-
-            return;
         }
+        return minNumOfFriends;
+    }
 
-        // 이번 DFS의 최대 인원까지 모든 순열을 찾아 workers에 add
-        for (int i = 0; i < dist.length; i++) {
+    private boolean dfs(int numOfFriends, int depth, boolean[] visited, int[] order) {
+        if (depth == numOfFriends) {
+            return canCheckAll(numOfFriends, order);
+        }
+        for (int i = 0; i < numOfFriends; i++) {
             if (visited[i]) {
                 continue;
             }
-
             visited[i] = true;
-            workers.add(i);
-            dfs(depth + 1, maxDepth, weak);
-            workers.remove(workers.size() - 1);
+            order[depth] = distances[i];
+            if (dfs(numOfFriends, depth + 1, visited, order)) {
+                return true;
+            }
             visited[i] = false;
         }
+        return false;
     }
 
-    boolean canFixAll(int[] weak) {
-        int idx = -1;
-        // 일꾼 한명 씩 벽을 고침
-        for (int worker : workers) {
-            int d = weak[idx + 1] + dist[worker];
-            // 새로 고칠 벽부터 마지막 벽까지
-            for (int i = idx + 1; i < weak.length; i++) {
-                // 고칠 수 있는 거리면 idx 교체, 고칠 수 없으면 break
-                if (weak[i] <= d) {
-                    idx = i;
-                } else {
-                    break;
+    private boolean canCheckAll(int numOfFriends, int[] order) {
+        boolean ret = false;
+        for (int i = 0; i < numOfWeekPoints; i++) {
+            int[] shiftedWeekPoints = getShiftedWeekPoints(i);
+            int idx = -1;
+            for (int j = 0; j < numOfFriends; j++) {
+                int limit = shiftedWeekPoints[idx + 1] + order[j];
+                for (int k = idx + 1; k < numOfWeekPoints; k++) {
+                    if (shiftedWeekPoints[k] <= limit) {
+                        idx = k;
+                    } else {
+                        break;
+                    }
                 }
             }
+            if (idx == numOfWeekPoints - 1) {
+                ret = true;
+                break;
+            }
         }
+        return ret;
+    }
 
-        if (idx == weak.length - 1) {
-            return true;
-        } else {
-            return false;
+    private int[] getShiftedWeekPoints(int distance) {
+        int[] shiftedWeekPoints = new int[numOfWeekPoints];
+        for (int j = 0; j < numOfWeekPoints - distance; j++) {
+            shiftedWeekPoints[j] = weekPoints[j + distance];
         }
+        for (int j = numOfWeekPoints - distance; j < numOfWeekPoints; j++) {
+            shiftedWeekPoints[j] = weekPoints[j + distance - numOfWeekPoints] + lenOfOuterWall;
+        }
+        return shiftedWeekPoints;
     }
 }
 
@@ -94,8 +92,8 @@ public class Test139 {
 
     public static void main(String[] args) {
         int n = 12;
-        int[] weak = {1, 5, 6, 10};
-        int[] dist = {1, 2, 3, 4};
+        int[] weak = {1, 3, 4, 9, 10};
+        int[] dist = {3, 5, 7};
         int answer = new Solution().solution(n, weak, dist);
         System.out.println(answer);
     }
