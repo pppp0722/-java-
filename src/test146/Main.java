72c533546398c02ef8d88bf50c3afb40452dea6c
ilhwan <poj0722@naver.com>
2023-02-07 22:41:13 +0900

    :sparkles: 투포/백준/골드5/2230 수 고르기

--- src/test146/Main.java
+++ src/test146/Main.java
@@ -1,41 +1,53 @@
 package test146;
-// 백준/골드5/2230 수 고르기
+// 투포/백준/골드5/2230 수 고르기
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    private static int n;
+    private static int m;
+    private static int[] arr;
+
     public static void main(String[] args) throws IOException {
+        init();
+        System.out.println(findMinDiff());
+    }
+
+    private static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] line = br.readLine().split(" ");
-        int N = Integer.parseInt(line[0]);
-        int M = Integer.parseInt(line[1]);
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(arr);
+        br.close();
+    }
 
+    private static int findMinDiff() {
+        int minDiff = Integer.MAX_VALUE;
+        Arrays.sort(arr);
         int l = 0;
         int r = 1;
-        int answer = Integer.MAX_VALUE;
-        while (l < N - 1 && r < N) {
+        while (l < n - 1 && r < n) {
             int diff = arr[r] - arr[l];
-            if (diff < M) {
+            if (diff < m) {
                 r++;
             } else {
-                answer = Math.min(diff, answer);
-                if(l + 1 != r) {
-                    l++;
-                } else {
+                minDiff = Math.min(diff, minDiff);
+                if (l + 1 == r) {
                     r++;
+                } else {
+                    l++;
                 }
             }
         }
-
-        System.out.println(answer);
+        return minDiff;
     }
 }
