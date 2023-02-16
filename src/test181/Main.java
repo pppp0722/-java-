c3daeb12c56f3e83c1d4bf64c4c3a737a4b63ce1
ilhwan <poj0722@naver.com>
2023-02-16 17:45:47 +0900

    :sparkles: LCS/백준/골드5/9251 LCS

--- src/test181/Main.java
+++ src/test181/Main.java
@@ -1,5 +1,5 @@
 package test181;
-// 백준/골드5/9251 LCS
+// LCS/백준/골드5/9251 LCS
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,20 +7,44 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    private static int len1;
+    private static int len2;
+    private static char[] chars1;
+    private static char[] chars2;
+
     public static void main(String[] args) throws IOException {
+        init();
+        System.out.println(findLenOfLCS());
+    }
+
+    private static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s1 = br.readLine();
-        String s2 = br.readLine();
-        int[][] lcs = new int[s1.length() + 1][s2.length() + 1];
-        for (int i = 1; i <= s1.length(); i++) {
-            for (int j = 1; j <= s2.length(); j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    lcs[i][j] = lcs[i - 1][j - 1] + 1;
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        len1 = str1.length();
+        len2 = str2.length();
+        chars1 = new char[len1 + 1];
+        chars2 = new char[len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            chars1[i] = str1.charAt(i - 1);
+        }
+        for (int i = 1; i <= len2; i++) {
+            chars2[i] = str2.charAt(i - 1);
+        }
+        br.close();
+    }
+
+    private static int findLenOfLCS() {
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (chars1[i] == chars2[j]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        System.out.println(lcs[s1.length()][s2.length()]);
+        return dp[len1][len2];
     }
 }
