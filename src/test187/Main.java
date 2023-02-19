310f3f23849753b16bb89bf63dba959626446e46
ilhwan <poj0722@naver.com>
2023-02-19 22:33:39 +0900

    :sparkles: 구현/백준/골드3/14890 경사로

--- src/test187/Main.java
+++ src/test187/Main.java
@@ -1,108 +1,76 @@
 package test187;
-// 백준/골드3/14890 경사로
+// 구현/백준/골드3/14890 경사로
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    private static int n;
+    private static int l;
+    private static int[][] map;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] line = br.readLine().split(" ");
-        int n = Integer.parseInt(line[0]);
-        int l = Integer.parseInt(line[1]);
-        int[][] map = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            map[i] = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        }
-
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            int h = map[i][0];
-            boolean check = true;
-            int support = 0;
-            for (int j = 0; j < n; j++) {
-                int cur = map[i][j];
-
-                if (support > 0) { // 아직 경사로가 남았을 때
-                    if (cur != h) {
-                        check = false;
-                        break;
-                    }
-                    support--;
-                } else { // 경사로 없을 때
-                    if (cur == h - 1) {
-                        if (j + l > n) {
-                            check = false;
-                            break;
-                        }
-                        h--;
-                        support = l - 1;
-                    } else if (cur == h + 1) {
-                        if (-support < l) {
-                            check = false;
-                            break;
-                        }
-                        h++;
-                        support = -1;
-                    } else if (cur != h) {
-                        check = false;
-                        break;
-                    } else {
-                        support--;
-                    }
-                }
-            }
-
-            if (check) {
-                answer++;
-            }
-
-            h = map[0][i];
-            check = true;
-            support = 0;
-            for (int j = 0; j < n; j++) {
-                int cur = map[j][i];
-
-                if (support > 0) { // 아직 경사로가 남았을 때
-                    if (cur != h) {
-                        check = false;
-                        break;
-                    }
-                    support--;
-                } else { // 경사로 없을 때
-                    if (cur == h - 1) {
-                        if (j + l > n) {
-                            check = false;
-                            break;
-                        }
-                        h--;
-                        support = l - 1;
-                    } else if (cur == h + 1) {
-                        if (-support < l) {
-                            check = false;
-                            break;
-                        }
-                        h++;
-                        support = -1;
-                    } else if (cur != h) {
-                        check = false;
-                        break;
-                    } else {
-                        support--;
-                    }
-                }
-            }
-
-            if (check) {
-                answer++;
-            }
-        }
-
-        System.out.println(answer);
+        init();
+        System.out.println(findNumOfPassableRoads());
     }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        l = Integer.parseInt(st.nextToken());
+        map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
+    private static int findNumOfPassableRoads() {
+        int numOfPassableRoads = 0;
+        for (int i = 0; i < n; i++) {
+            if (canPass(i, Direction.HORIZONTAL)) {
+                numOfPassableRoads++;
+                System.out.println("수평" + i);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (canPass(i, Direction.VERTICAL)) {
+                numOfPassableRoads++;
+                System.out.println("수직" + i);
+            }
+        }
+        return numOfPassableRoads;
+    }
+
+    private static boolean canPass(int idx, Direction direction) {
+        int[] arr;
+        if (direction == Direction.HORIZONTAL) {
+            arr = map[idx];
+        } else {
+            arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = map[i][idx];
+            }
+        }
+        return canPass(arr);
+    }
+
+    private static boolean canPass(int[] arr) {
+        boolean canPass = true;
+        int[] support = new int[n];
+        int h = arr[0];
+        for (int i = 1; i < n; i++) {
+        }
+        return canPass;
+    }
+}
+
+enum Direction {
+    HORIZONTAL, VERTICAL
 }
