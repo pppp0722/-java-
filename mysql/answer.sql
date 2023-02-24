b29be933e84464f56527cfcf52b11b83bc5e1e8c
ilhwan <poj0722@naver.com>
2023-02-24 22:49:24 +0900

    :sparkles: SUM,MAX,MIN/프로그래머스/Level2/동물 수 구하기

--- mysql/answer.sql
+++ mysql/answer.sql
@@ -6,3 +6,7 @@ from animal_ins;
 select count(distinct name) as 'count'
 from animal_ins
 where name is not null;
+
+-- SUM,MAX,MIN/프로그래머스/Level2/동물 수 구하기
+select count(animal_id) as 'count'
+from animal_ins;
