ac9de3e54221be5a55723b120061895890d4f3d5
ilhwan <poj0722@naver.com>
2023-02-24 22:45:58 +0900

    :sparkles: SUM,MAX,MIN/프로그래머스/Level2/중복 제거하기

--- mysql/answer.sql
+++ mysql/answer.sql
@@ -1,3 +1,8 @@
 -- SUM,MAX,MIN/프로그래머스/Level2/최솟값 구하기
 select min(datetime) as '시간'
 from animal_ins;
+
+-- SUM,MAX,MIN/프로그래머스/Level2/중복 제거하기
+select count(distinct name)
+from animal_ins
+where name is not null;
