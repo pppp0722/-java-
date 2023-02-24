1e67edc7da377bc33a9c55b8a3d3b92a1d221343
ilhwan <poj0722@naver.com>
2023-02-24 22:53:31 +0900

    :sparkles: IS NULL/프로그래머스/Level1/이름이 있는 동물의 아이디

--- mysql/answer.sql
+++ mysql/answer.sql
@@ -10,3 +10,9 @@ where name is not null;
 -- SUM,MAX,MIN/프로그래머스/Level2/동물 수 구하기
 select count(animal_id) as 'count'
 from animal_ins;
+
+-- IS NULL/프로그래머스/Level1/이름이 있는 동물의 아이디
+select animal_id
+from animal_ins
+where name is not null
+order by animal_id asc;
