c87c8cab8c573c936ef3512d54b17b600f502376
ilhwan <poj0722@naver.com>
2023-03-01 18:32:17 +0900

    :sparkles: JOIN/프로그래머스/Level3/오랜 기간 보호한 동물(1)

--- mysql/answer.sql
+++ mysql/answer.sql
@@ -142,3 +142,10 @@ from animal_outs
 where hour(datetime) >= 9 and hour(datetime) < 20
 group by hour
 order by hour asc;
+
+-- JOIN/프로그래머스/Level3/오랜 기간 보호한 동물(1)
+select a.name, a.datetime
+from animal_ins a
+left join animal_outs b on a.animal_id = b.animal_id
+where b.animal_id is null
+order by a.datetime asc limit 3;
