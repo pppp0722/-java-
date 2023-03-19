5142420fef83ea990080f3e0707959ec2108a83f
ilhwan <poj0722@naver.com>
2023-03-19 23:48:21 +0900

    :sparkles: 그리디/백준/골드3/2457 공주님의 정원

--- src/test116/Main.java
+++ src/test116/Main.java
@@ -1,60 +1,80 @@
 package test116;
-// 백준/골드3/2457 공주님의 정원
+// 그리디/백준/골드3/2457 공주님의 정원
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static final int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final int[] DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final int START_DAY = getDay(3, 1);
+    private static final int END_DAY = getDay(11, 30);
+    private static PriorityQueue<Flower> flowers;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] endDays = new int[366];
-        for (int i = 0; i < N; i++) {
-            int[] arr = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-            int start = getDay(arr[0], arr[1]);
-            int end = getDay(arr[2], arr[3]);
-
-            endDays[start] = Math.max(end, endDays[start]);
-        }
-
-        int lastDay = 1;
-        int day = 60;
-        int ct = 0;
-        while (day < 335) {
-            int maxDay = 0;
-            for (int i = lastDay; i <= day; i++) {
-                maxDay = Math.max(endDays[i], maxDay);
-            }
-
-            if (maxDay == 0) {
-                break;
-            }
-
-            lastDay = day;
-            day = maxDay;
-            ct++;
-        }
-
-        if (day >= 335) {
-            System.out.println(ct);
-        } else {
-            System.out.println(0);
-        }
+        init();
+        System.out.println(findMin());
     }
 
-    static int getDay(int month, int day) {
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        flowers = new PriorityQueue<>(Comparator.comparingInt(f -> f.startDay));
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int startMonth = Integer.parseInt(st.nextToken());
+            int startDay = Integer.parseInt(st.nextToken());
+            int endMonth = Integer.parseInt(st.nextToken());
+            int endDay = Integer.parseInt(st.nextToken());
+            flowers.add(new Flower(getDay(startMonth, startDay), getDay(endMonth, endDay)));
+        }
+        br.close();
+    }
+
+    private static int getDay(int month, int day) {
         int ret = 0;
         for (int i = 1; i < month; i++) {
-            ret += days[i];
+            ret += DAYS[i];
         }
-
         return ret + day;
+    }
+
+    private static int findMin() {
+        int ct = 0;
+        int curDay = START_DAY;
+        PriorityQueue<Flower> pq = new PriorityQueue<>((f1, f2) -> f2.endDay - f1.endDay);
+        while (!flowers.isEmpty()) {
+            while (!flowers.isEmpty() && flowers.peek().startDay <= curDay) {
+                pq.offer(flowers.poll());
+            }
+            if (pq.isEmpty()) {
+                break;
+            }
+            Flower nextFlower = pq.poll();
+            curDay = nextFlower.endDay;
+            ct++;
+            if (curDay > END_DAY) {
+                break;
+            }
+        }
+        if (curDay <= END_DAY) {
+            ct = 0;
+        }
+        return ct;
+    }
+}
+
+class Flower {
+
+    public int startDay;
+    public int endDay;
+
+    public Flower(int startDay, int endDay) {
+        this.startDay = startDay;
+        this.endDay = endDay;
     }
 }
