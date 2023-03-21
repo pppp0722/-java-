bea11a1813da2beafe91aca1dcc09bada0f9a842
ilhwan <poj0722@naver.com>
2023-03-21 22:02:03 +0900

    :sparkles: 트리/백준/골드5/15681 트리와 쿼리

---
+++ src/test270/Main.java
@@ -0,0 +1,66 @@
+package test270;
+// 트리/백준/골드5/15681 트리와 쿼리
+
+import static java.lang.System.lineSeparator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int n;
+    private static int r;
+    private static int q;
+    private static List<List<Integer>> tree;
+    private static int[] queryArr;
+    private static int[] numOfNodesArr;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        findNumOfNodes(r, 0);
+        StringBuilder answer = new StringBuilder();
+        for (int query : queryArr) {
+            answer.append(numOfNodesArr[query]).append(lineSeparator());
+        }
+        System.out.println(answer);
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+        tree = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            tree.add(new ArrayList<>());
+        }
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            tree.get(u).add(v);
+            tree.get(v).add(u);
+        }
+        queryArr = new int[q];
+        for (int i = 0; i < q; i++) {
+            queryArr[i] = Integer.parseInt(br.readLine());
+        }
+        numOfNodesArr = new int[n + 1];
+        br.close();
+    }
+
+    private static int findNumOfNodes(int node, int preNode) {
+        int numOfNodes = 1; // 자기 자신
+        tree.get(node).removeIf(e -> e == preNode);
+        for (int nextNode : tree.get(node)) {
+            numOfNodes += findNumOfNodes(nextNode, node);
+        }
+        numOfNodesArr[node] = numOfNodes;
+        return numOfNodes;
+    }
+}
