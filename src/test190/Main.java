74ed1cb478f270fc4de27309a0758c2e16c73a62
ilhwan <poj0722@naver.com>
2023-04-10 22:33:13 +0900

    :sparkles: 트리맵/백준/골드4/7662 이중 우선순위 큐

--- src/test190/Main.java
+++ src/test190/Main.java
@@ -1,27 +1,23 @@
 package test190;
-// 백준/골드4/7662 이중 우선순위 큐
+// 트리맵/백준/골드4/7662 이중 우선순위 큐
+
+import static java.lang.System.lineSeparator;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < t; i++) {
             int k = Integer.parseInt(br.readLine());
-            PriorityQueue<Integer> minPQ = new PriorityQueue<>();
-            PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Comparator.reverseOrder());
-            Map<Integer, Integer> map = new HashMap<>();
-            int iCt = 0;
-            int dCt = 0;
+            TreeMap<Integer, Integer> treeMap = new TreeMap<>();
 
             for (int j = 0; j < k; j++) {
                 String[] line = br.readLine().split(" ");
@@ -29,33 +25,24 @@ public class Main {
                 int num = Integer.parseInt(line[1]);
 
                 if (command == 'I') {
-                    minPQ.offer(num);
-                    maxPQ.offer(num);
-                    map.put(num, map.getOrDefault(num, 0) + 1);
-                    iCt++;
-                } else if (iCt > dCt) {
-                    int removed = remove(num == -1 ? minPQ : maxPQ, map);
-                    map.put(removed, map.get(removed) - 1);
-                    dCt++;
+                    treeMap.put(num, treeMap.getOrDefault(num, 0) + 1);
+                } else if (!treeMap.isEmpty()) {
+                    int key = num == 1 ? treeMap.lastKey() : treeMap.firstKey();
+                    treeMap.put(key, treeMap.get(key) - 1);
+                    if (treeMap.get(key) == 0) {
+                        treeMap.remove(key);
+                    }
                 }
             }
 
-            if (iCt == dCt) {
-                System.out.println("EMPTY");
+            if (treeMap.isEmpty()) {
+                sb.append("EMPTY");
             } else {
-                minPQ.offer(remove(minPQ, map));
-                maxPQ.offer(remove(maxPQ, map));
-                System.out.println(maxPQ.poll() + " " + minPQ.poll());
+                sb.append(treeMap.lastKey()).append(" ").append(treeMap.firstKey());
             }
+            sb.append(lineSeparator());
         }
-    }
 
-    public static int remove(PriorityQueue<Integer> pq, Map<Integer, Integer> map) {
-        int removed;
-        do {
-            removed = pq.poll();
-        } while (pq.size() > 0 && map.get(removed) == 0);
-
-        return removed;
+        System.out.println(sb);
     }
 }
